eacd4b412519d0eae0c7cad8178e97d4c4c173fb
Phil VanVoorhis <philv2010@gmail.com>
2019-11-13 20:10:20 -0600

    attempt 6 for the branch picker

--- scripts/TCM-global2.groovy
+++ scripts/TCM-global2.groovy
@@ -1,4 +1,10 @@
-properties([ buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), parameters([credentials(credentialType: 'com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl', defaultValue: 'b5ac603a-6b93-4255-bcaf-eeed3e27698e', description: 'TCM AEM Deploy User', name: 'JENKINS_DEPLOY_USER', required: true)]), ([[$class: 'GitParameterDefinition', branch: '', branchFilter: '.*', defaultValue: '', description: '', name: 'Branch', quickFilterEnabled: false, selectedValue: 'TOP', sortMode: 'DESCENDING_SMART', tagFilter: '*', type: 'PT_BRANCH']])])
+properties(
+	[ buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), 
+parameters(
+	[credentials(credentialType: 'com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl', defaultValue: 'b5ac603a-6b93-4255-bcaf-eeed3e27698e', description: 'TCM AEM Deploy User', name: 'JENKINS_DEPLOY_USER', required: true)], 
+	[$class: 'GitParameterDefinition', branch: '', branchFilter: '.*', defaultValue: '', description: '', name: 'Branch', quickFilterEnabled: false, selectedValue: 'TOP', sortMode: 'DESCENDING_SMART', tagFilter: '*', type: 'PT_BRANCH']
+	)]
+	)
 
 node("master") {
     def workspace = pwd()
